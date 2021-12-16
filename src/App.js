a5e0d09b952684a74aa91fbdc141bcac61e016ac
Bruno Martín Pedraza <brunompedraza@gmail.com>
2021-12-16 10:16:51 -0300

    Ultimo update

--- src/App.js
+++ src/App.js
@@ -12,13 +12,19 @@ import bobcara from './assets/characters/bobface.png'
 import safaricara from './assets/characters/safarigirlface.png'
 import bob from './assets/characters/bob.png'
 import safarigirl from './assets/characters/safarigirl.png'
+import salooncara from './assets/characters/saloongirlface.png'
+import saloongirl from './assets/characters/saloongirl.png'
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <>
       <Board/>
-        <Pj imagen={bob}/>
-        <Pj imagen={safarigirl} />
+        <Pj nombre={'bob'} imagenDeCara={bobcara} imagen={bob}/>
+        <Pj nombre={'safari'} imagenDeCara={safaricara} imagen={safarigirl} />
+        <Pj nombre={'saloongirl'} imagenDeCara={salooncara} imagen={saloongirl}/>
       
       {/* <Person nombre="Brunete" imagenCompleta={safarigirl} imagenCara={safaricara}/>
       <Person nombre="Betunia"  imagenCompleta={bob} imagenCara={bobcara}/>
--- src/components/Pj.jsx
+++ src/components/Pj.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import { Button, Modal } from "react-bootstrap";
+import React,{useState} from "react";
 
-const Pj = ({imagen}) => {
-    return (
-        <div>
-           <img 
-           className='person' 
-           src={imagen} 
-           alt="" 
-           /> 
-        </div>
-    )
-}
 
-export default Pj
+const Pj = ({ imagen, nombre,imagenDeCara }) => {
+    const [smShow, setSmShow] = useState(false);
+
+  return (
+    <div>
+      <img onClick={() => setSmShow(true)} className={nombre} src={imagen} alt="" />
+
+      <Modal
+        size="sm"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            {nombre}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <img src={imagenDeCara} alt="" />
+        </Modal.Body>
+      </Modal>
+
+    </div>
+  );
+};
+
+export default Pj;
